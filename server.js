fe302e87533775178c9b709972c24a7ec24210f3
Alquipo Neto <alquiponeto@outlook.com.br>
2020-06-09 20:53:34 -0300

    BD connection

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
-const express = require("express")
+const express = require('express')
+const mongoose = require('mongoose')
 
+//iniciando APp
 const app = express()
 
+//iniciando o db
+mongoose.connect('mongodb://localhost:27017/apiProductHunt', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+
+//Primeira Roda
 app.get('/', (request, response) => {
     response.send('Hello World')
 });
