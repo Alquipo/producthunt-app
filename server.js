d2434d400a35e31453b8ad1b5e490f9cda88de50
Alquipo Neto <alquiponeto@outlook.com.br>
2020-06-09 16:55:19 -0300

    Script Nodemon

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express")
 const app = express()
 
 app.get('/', (request, response) => {
-    res.send('Hello World')
+    response.send('Hello World')
 });
 
 app.listen(3001)
