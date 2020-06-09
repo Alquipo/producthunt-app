16b804de97805860b4aa4ad7b298782613ab522a
Alquipo Neto <alquiponeto@outlook.com.br>
2020-06-09 16:39:54 -0300

    nodemon install

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
-const express = require('express')
+const express = require("express")
 
 const app = express()
+
+app.get('/', (request, response) => {
+    res.send('Hello World')
+});
 
 app.listen(3001)
