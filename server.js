084480ee0352288255cf4d1a8de82d737488d61b
Alquipo Neto <alquiponeto@outlook.com.br>
2020-06-09 21:22:44 -0300

    Schema Product

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const requireDir = require('require-dir')
 
 //iniciando APp
 const app = express()
@@ -10,10 +11,19 @@ mongoose.connect('mongodb://localhost:27017/apiProductHunt', {
     useUnifiedTopology: true
 })
 
+requireDir('./src/models')
+
+const Product = mongoose.model('Product')
 
 //Primeira Roda
 app.get('/', (request, response) => {
-    response.send('Hello World')
+    Product.create({
+        title: 'React Native',
+        description: 'Build native apps with React',
+        url: 'http://github.com/facebook/react-native'
+    })
+
+    return response.send('Hello World')
 });
 
 app.listen(3001)
